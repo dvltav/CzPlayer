83b69c7be81219cbd18c9b48ec1210e5d4d5a8d9
dvltav <dominik@Dominiks-MacBook-Pro.local>
2015-10-10 07:33:11 -0700

    fix

--- CzPlayer/RadioPlayer.swift
+++ CzPlayer/RadioPlayer.swift
@@ -10,15 +10,16 @@ import Foundation
 import AVFoundation
 
 class RadioPlayer {
+    
     static let sharedInstance = RadioPlayer()
-    //private var player = AVPlayer(URL: NSURL(string: "http://www.radiobrasov.ro/listen.m3u")!)
-    //http://icecast5.play.cz/impuls128.mp3
+    
     private var player = AVPlayer(URL: NSURL(string: "http://icecast5.play.cz/impuls128.mp3")!)
     private var isPlaying = false
     
     func play() {
         player.play()
         isPlaying = true
+        player.
     }
     
     func pause() {
